7d571a36098c966e02256b1adf39690da1d5517f
Joel DSouza <joel.dsouza@seamless.se>
2022-03-29 18:39:33 +0300

    added jest testing to react template

---
+++ template-react/src/App.test.tsx
@@ -0,0 +1,18 @@
+import App from './App';
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+
+test('Renders main page correctly', async () => {
+  // setup
+  render(<App />);
+  const buttonCount = await screen.findByRole('button');
+
+  // pre expectations
+  expect(buttonCount.innerHTML).toBe('count is: 0');
+
+  // init
+  user.click(buttonCount);
+
+  // post expectations
+  expect(buttonCount.innerHTML).toBe('count is: 2');
+});
